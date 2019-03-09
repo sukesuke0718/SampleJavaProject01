d2e47925699ddf73e040f6a731375f1d65d66b07
ユウ <sukesuke0718@gmail.com>
2019-03-09 23:56:33 +0900

    new

---
+++ src/jankenSample/ObjectJanken.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jankenSample;
+
+/**
+ * @author ユウスケ
+ *
+ */
+public class ObjectJanken {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
